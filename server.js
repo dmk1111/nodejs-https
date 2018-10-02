1730dcf5253c87fff3c524ef711422067de10445
dmk1111 <dima.golysh@gmail.com>
2018-10-02 17:17:50 +0300

    feat: use openshiftapps domain, code formatting

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ const lex = require('greenlock-express').create({
     version: 'draft-11',
     server: PROD ? 'https://acme-v02.api.letsencrypt.org/directory' : 'https://acme-staging-v02.api.letsencrypt.org/directory',
     approveDomains: (opts, certs, cb) => {
-    if (certs) {
-        // change domain list here
-        opts.domains = ['example.com', 'yourdomain.com', 'localhost', '127.0.0.1']
-    } else {
-        // change default email to accept agreement
-        opts.email = 'dimagolysh@gmail.com';
-opts.agreeTos = true;
-}
-cb(null, { options: opts, certs: certs });
-}
+        if (certs) {
+            // change domain list here
+            opts.domains = ['secure-node-testing-secure-node-testing.7e14.starter-us-west-2.openshiftapps.com']
+        } else {
+            // change default email to accept agreement
+            opts.email = 'dimagolysh@gmail.com';
+            opts.agreeTos = true;
+        }
+        cb(null, { options: opts, certs: certs });
+    }
 // optional: see "Note 3" at the end of the page
 // communityMember: true
 });
